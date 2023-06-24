2373b09957d795cdef25b8029fc0e4917671bd64
ClaudiaSolomon <claudia.solomon39@yahoo.com>
2023-06-24 17:29:06 +0300

    Update Connect.php

--- proiect/auth/Connect.php
+++ proiect/auth/Connect.php
@@ -1,17 +1,42 @@
 <?php
+class DatabaseConnection
+{
+    private $host;
+    private $dbname;
+    private $username;
+    private $password;
+    private $mysqli;
 
-$host = "localhost";
-$dbname = "web";
-$username = "root";
-$password = "";
+    public function __construct($host, $dbname, $username, $password)
+    {
+        $this->host = $host;
+        $this->dbname = $dbname;
+        $this->username = $username;
+        $this->password = $password;
 
-$mysqli = new mysqli(hostname: $host,
-    username: $username,
-    password: $password,
-    database: $dbname);
+        $this->connect();
+    }
 
-if ($mysqli->connect_errno) {
-    die("Connection error: " . $mysqli->connect_error);
+    private function connect()
+    {
+        $this->mysqli = new mysqli($this->host, $this->username, $this->password, $this->dbname);
+
+        if ($this->mysqli->connect_errno) {
+            die("Connection error: " . $this->mysqli->connect_error);
+        }
+    }
+
+    public function getConnection()
+    {
+        return $this->mysqli;
+    }
 }
 
+$host = "161.35.71.86";
+$dbname = "web";
+$username = "hcode";
+$password = "M976Yn^63Eu?pq27";
+
+$database = new DatabaseConnection($host, $dbname, $username, $password);
+$mysqli = $database->getConnection();
 return $mysqli;
